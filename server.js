cdbcf8423558e15346485b712d8f785a077775b9
ladyjem <ladyjem@gmail.com>
2017-11-03 20:53:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-
+ 
 // Mongoose internally uses a promise-like object,
 // but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
