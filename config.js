ac2dc426a5ae53bb8f91bb55e35ec0d981770922
Jem Corraggio <ladyjem@gmail.com>
2017-11-04 00:58:08 +0000

    mine

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/restaurants-app';
+                      'mongodb://ladyjem:bestrong9@ds239965.mlab.com:39965/first-database';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/test-restaurants-app');
+	'mongodb://ladyjem:bestrong9@ds241875.mlab.com:41875/test-restaurants-app');
 exports.PORT = process.env.PORT || 8080;
